80bdedd6707b91285c2d1a4ed59d4aa57c3d4318
Matheus Henrique Bonfim da Silva <104633582+RexDetona@users.noreply.github.com>
2023-04-27 12:24:41 -0300

    Mudança no Registro
    Arrumado o Registro assim melhorando a responsividade conforme a tela fica pequena.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,44 @@
 window.addEventListener("load", function(){
-    const element = document.getElementById("");
-    if(window.innerWidth < 1350){
-        element.style.transform = "translateX(0)"
-    }else{
+    const element = document.getElementById("QuadPai");
+    if(window.innerWidth >  1350){
         element.style.transform = "translateX(0)"
     }
 })
 
-window.addEventListener("resize", function(){
+window.addEventListener("load", function(){
     const element = document.getElementById("QuadPai");
-    if(window.innerWidth > 1121 & window.innerWidth < 1350){
+    if(window.innerWidth <  1350 & window.innerWidth >  1181 ){
         element.style.transform = "translateX(-5vh)"
-    }else{
+    }
+})
+
+window.addEventListener("load", function(){
+    const element = document.getElementById("QuadPai");
+    if(window.innerWidth < 1180 & window.innerWidth > 971){
+        element.style.transform = "translateX(-10vh)"
+    }
+})
+
+window.addEventListener("resize", function(){
+    const element = document.getElementById("QuadPai");
+    if(window.innerWidth >  1350){
         element.style.transform = "translateX(0)"
     }
 })
 
 window.addEventListener("resize", function(){
     const element = document.getElementById("QuadPai");
-    if(window.innerWidth < 921 & window.innerWidth < 1120){
-        element.style.transform = "translateX(-10vh)"
-    }else{
-        element.style.transform = "translateX(0)"
+    if(window.innerWidth <  1350 & window.innerWidth >  1181 ){
+        element.style.transform = "translateX(-5vh)"
     }
 })
+
 window.addEventListener("resize", function(){
     const element = document.getElementById("QuadPai");
-    if(window.innerWidth < 920){
-        element.style.transform = "translateX(-15vh)"
-    }else{
-        element.style.transform = "translateX(0)"
+    if(window.innerWidth < 1180 & window.innerWidth > 971){
+        element.style.transform = "translateX(-10vh)"
     }
 })
+
+
 
