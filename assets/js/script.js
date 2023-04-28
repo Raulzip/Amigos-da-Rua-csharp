1c6e585cc0f8f0086325d7a1b66a43060b6c5581
Matheus Henrique Bonfim da Silva <104633582+RexDetona@users.noreply.github.com>
2023-04-28 17:26:25 -0300

    Mudanças no Registro
    Adicionado o footer ao registro e agora a tela de registro tem um css separado para tal, onde acontece as mudanças de @media.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,4 @@
-window.addEventListener("load", function(){
-    const element = document.getElementById("QuadPai");
-    if(window.innerWidth >  1350){
-        element.style.transform = "translateX(0)"
-    }
-})
 
-window.addEventListener("load", function(){
-    const element = document.getElementById("QuadPai");
-    if(window.innerWidth <  1350 & window.innerWidth >  1181 ){
-        element.style.transform = "translateX(-5vh)"
-    }
-})
-
-window.addEventListener("load", function(){
-    const element = document.getElementById("QuadPai");
-    if(window.innerWidth < 1180 & window.innerWidth > 971){
-        element.style.transform = "translateX(-10vh)"
-    }
-})
-
-window.addEventListener("resize", function(){
-    const element = document.getElementById("QuadPai");
-    if(window.innerWidth >  1350){
-        element.style.transform = "translateX(0)"
-    }
-})
-
-window.addEventListener("resize", function(){
-    const element = document.getElementById("QuadPai");
-    if(window.innerWidth <  1350 & window.innerWidth >  1181 ){
-        element.style.transform = "translateX(-5vh)"
-    }
-})
-
-window.addEventListener("resize", function(){
-    const element = document.getElementById("QuadPai");
-    if(window.innerWidth < 1180 & window.innerWidth > 971){
-        element.style.transform = "translateX(-10vh)"
-    }
-})
 
 
 
