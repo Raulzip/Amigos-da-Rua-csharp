1fc2ef89b7a4ea8af503fc1fb21208aed2d2861f
Matheus Henrique Bonfim da Silva <104633582+RexDetona@users.noreply.github.com>
2023-05-05 13:15:04 -0300

    Adicionando Validação
    Em processo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
+const Nome = document.getElementById("NomeCad")
+const Email = document.getElementById("EmailCad")
+const Senha = document.getElementById("SenhaCad")
+const ConSenha = document.getElementById("ConSenhaCad")
+const Botao = document.getElementById("BotaoCad")
 
 
+function verificarCampos() {
+  if(Nome.value !== "" && Email.value !== "" && Senha.value !== "" && ConSenha.value !== "" && Senha.value === ConSenha.value) {
+    Botao.setAttribute("data-bs-target", "#staticBackdrop")
+  } else {
+    Botao.setAttribute("data-bs-target", "#staticBackdropErr")
+  }
+}
 
 
+Nome.addEventListener("input", verificarCampos)
+Email.addEventListener("input", verificarCampos)
+Senha.addEventListener("input", verificarCampos)
+ConSenha.addEventListener("input", verificarCampos)
